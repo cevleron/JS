ce7eeffc64f335a160c5c90ef1575e24cd02c8a1
Anton <gancher9@gmail.com>
2021-07-21 08:18:50 +0300

    Lesson 18

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмоф вы уже посмотрели?', '');
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмоф вы уже посмотрели?', '');
+    
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        
+    }
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -10,15 +19,128 @@ const personalMovieDB = {
     privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
     
-    personalMovieDB.movies[a] = b;
+        if (a != null && b != null && a != '' && b  != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console .log('Вы классный зритель')
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман')
+} else {
+    console.log('Произошла ошибка');
+}
 }
 
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            personalMovieDB.genres[i - 1] = genre;
+        } 
+    
+}
+
+writeYourGenres();
 
 console.log(personalMovieDB);
+
+
+
+// Урок 17. Методы и свойства строк и чисел
+
+
+// const str = 'test';
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str);
+
+// const fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('fruit'));
+
+// const logg = 'hello world';
+
+// // console.log(logg.slice(6, 11));
+
+// // console.log(logg.substring(6,11));
+
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
+
+
+
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+// }
+
+// showFirstMessage('Hello  World!');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function () {
+//   console.log('Hello')  
+// };
+// logger();
+
+
+// const calc = (a, b) => { return a + b };
+    
+
+
+
+
 
 // if (4 == 9) {
 //     console.log('Ok!');
